891d8ffd1289ff199c8f40546b3fa49f3a956faf
Trevian <trevian2345@gmail.com>
2020-03-05 20:09:51 -0500

    Added backtrack from goal

--- Dijkstra_point.py
+++ Dijkstra_point.py
@@ -27,8 +27,8 @@ class PointRobot:
                         [1, 0, 1],
                         [1, 1, sqrt(2)]]
         self.map = ObstacleMap()  # Instance of the map object
-        self.start = start  # Starting cordinates in tuple form
-        self.goal = goal  # Goal cordinates in tuple form
+        self.start = start  # Starting coordinates in tuple form
+        self.goal = goal  # Goal coordinates in tuple form
 
         # Check to see if start and goal cells lie within map boundaries
         if not (0 <= self.start[0] < self.map.height) or not (0 <= self.start[1] < self.map.width):
@@ -50,17 +50,17 @@ class PointRobot:
         self.openList = []  # List of coordinates to be explored, in the form: [(y, x), cost, action]
         self.openGrid = np.zeros_like(self.map.image, dtype=np.uint8)  # Grid of cells pending exploration
         self.closeGrid = np.zeros_like(self.map.image, dtype=np.uint8)  # Grid of explored cells
-        self.actionGrid = np.zeros_like(self.map.image, dtype=np.uint8)  # Grid containing movement policy
+        self.actionGrid = np.zeros_like(self.map.image, dtype=np.uint8) + 255  # Grid containing movement policy
+        self.backTrack = cv2.cvtColor(np.copy(self.map.image), cv2.COLOR_GRAY2RGB)  # Image of the optimal path
         self.solve()
 
     def solve(self):
         # Initialize the open list/grid with the start cell
-        self.openList = [[self.start, 0, -1]]  # [point, cost, action]
+        self.openList = [[self.start, 0, 255]]  # [point, cost, action]
         self.openGrid[self.start] = 1
         sys.stdout.write("\nStarting path finder...\n")
         while len(self.openList) > 0:
             # Find index of minimum cost cell
-            # print(self.openList)
             cost_list = [self.openList[i][1] for i in range(len(self.openList))]
             index = int(np.argmin(cost_list, axis=0))
             cell = self.openList[index][0]
@@ -73,47 +73,61 @@ class PointRobot:
                 self.openList = []
 
             # Expand cell
-            for a in range(len(self.actions)):
-                next_cell = (cell[0] + self.actions[a][0], cell[1]+self.actions[a][1])
-                # Check for map boundaries
-                if 0 <= next_cell[0] < self.map.height and 0 <= next_cell[1] < self.map.width:
-                    # Check for obstacles
-                    if not self.map.image[next_cell[0], next_cell[1]]:
-                        # Check whether cell has been explored
-                        if not self.closeGrid[next_cell[0], next_cell[1]]:
-                            # Check if cell is already pending exploration
-                            if not self.openGrid[next_cell[0], next_cell[1]]:
-                                self.openList.append([next_cell, cost + self.actions[a][2], a])
-                                self.openGrid[next_cell] = 1
-                            else:
-                                # TODO:  Handle open cell being approached from two cells with same cumulative cost
-                                pass
+            else:
+                for a in range(len(self.actions)):
+                    next_cell = (cell[0] + self.actions[a][0], cell[1]+self.actions[a][1])
+                    # Check for map boundaries
+                    if 0 <= next_cell[0] < self.map.height and 0 <= next_cell[1] < self.map.width:
+                        # Check for obstacles
+                        if not self.map.image[next_cell[0], next_cell[1]]:
+                            # Check whether cell has been explored
+                            if not self.closeGrid[next_cell[0], next_cell[1]]:
+                                # Check if cell is already pending exploration
+                                if not self.openGrid[next_cell[0], next_cell[1]]:
+                                    self.openList.append([next_cell, cost + self.actions[a][2], a])
+                                    self.openGrid[next_cell] = 1
+                                else:
+                                    # TODO:  Handle open cell being approached from two cells with same cumulative cost
+                                    pass
+                self.openList.pop(index)
 
             # Mark the cell as having been explored
             self.openGrid[cell] = 0
             self.closeGrid[cell] = 1
             self.actionGrid[cell] = action
-            self.openList.pop(index)
             cv2.imshow("Maze", self.closeGrid * 255)
             cv2.waitKey(1)
+        cv2.destroyWindow("Maze")
 
-        # TODO:  Implement backtrack and animation
-        pass
+        # Backtracking from the goal cell to extract an optimal path
+        current_cell = self.goal
+        next_action_index = self.actionGrid[current_cell]
+        while next_action_index != 255:
+            current_cell = (current_cell[0] - self.actions[next_action_index][0],
+                            current_cell[1] - self.actions[next_action_index][1])
+            self.backTrack[current_cell] = (255, 0, 0)
+            next_action_index = self.actionGrid[current_cell]
+        cv2.imshow("Goal", self.backTrack + cv2.cvtColor(self.map.image * 254, cv2.COLOR_GRAY2RGB))
+        cv2.waitKey(0)
+        cv2.destroyWindow("Goal")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Project 2:  Naviagtion of a point robot from a start point to an"
+    parser = argparse.ArgumentParser(description="Project 2:  Navigation of a point robot from a start point to an"
                                                  "end point")
-    parser.add_argument('initialx', type=int, help='X-coordinate of Initial node of the robot')
-    parser.add_argument('initialy', type=int, help='Y-coordinate of Initial node of the robot')
-    parser.add_argument('goalx', type=int, help='X-coordinate of Goal node of the robot')
-    parser.add_argument('goaly', type=int, help='Y-coordinate of Goal node of the robot')
+    parser.add_argument('initialX', type=int, help='X-coordinate of Initial node of the robot')
+    parser.add_argument('initialY', type=int, help='Y-coordinate of Initial node of the robot')
+    parser.add_argument('goalX', type=int, help='X-coordinate of Goal node of the robot')
+    parser.add_argument('goalY', type=int, help='Y-coordinate of Goal node of the robot')
+    parser.add_argument('--radius', type=int, metavar="R", help='Indicates that the robot is rigid, with radius R')
+    parser.add_argument("--clearance", type=int, metavar="C",
+                        help="Indicates that the robot requires a clearance of at least C pixels")
     args = parser.parse_args()
 
-    sx = args.initialx
-    sy = args.initialy
-    gx = args.goalx
-    gy = args.goaly
+    sx = args.initialX
+    sy = args.initialY
+    gx = args.goalX
+    gy = args.goalY
     start_pos = (sx, sy)
     goal_pos = (gx, gy)
-    pointrob = PointRobot(start_pos, goal_pos)
+    pointRobot = PointRobot(start_pos, goal_pos)
